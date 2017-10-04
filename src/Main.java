d7bdfc2b261a2478bf7324b1940553795c01c881
Indiver Kumar <ink@censhare.com>
2017-10-04 14:04:00 +0530

    co mit7

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,6 @@ public class Main {
 		System.out.println("Git testing4");
 		System.out.println("Git testing5");
 		System.out.println("Git testing6commit");
+		System.out.println("Git testing7");
 	}
 }
